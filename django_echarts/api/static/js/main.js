611247476a1b64723777f5828a41cd37d7a78cea
juan <lagojuanfrancisco@gmail.com>
2023-04-02 15:03:21 -0300

    Tutorial finalizado

---
+++ django_echarts/api/static/js/main.js
@@ -0,0 +1,20 @@
+const getOptionChart = async () => {
+    try {
+        const response = await fetch("http://127.0.0.1:8000/api/get_chart/");
+        return await response.json();
+    } catch (ex) {
+        alert(ex);
+    }
+};
+
+
+const initChart = async () => {
+    const myChart = echarts.init(document.getElementById("chart"));
+
+    myChart.setOption(await getOptionChart());
+};
+
+
+window.addEventListener("load", async () => {
+    await initChart();
+});
